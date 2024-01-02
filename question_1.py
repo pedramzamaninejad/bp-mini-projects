bd1573baff719b74ad0c1557ef495296f0c70e83
pedram.zamaninezhad <pzamaninejad@yahoo.com>
2024-01-02 13:12:50 +0330

    refactor question 1 algorithm

--- question_1.py
+++ question_1.py
@@ -1,24 +1,29 @@
 def tajzie(number):
     avamel = {}
+    original_num = number
+    while number % 2 == 0:
+        if not avamel.get('2'):
+            avamel['2'] = 0
+        avamel['2'] += 1
+        number /= 2
     # تست کردن تمام اعداد کمتر از نصف زیرا اگر عددی برای مثال به دو بخش پذیر باشد
     # پس بقیه عوامل آن هم کمتر از مکمل دو است
-    for i in range(2, number // 2 + 1):
-        reminder = True
+    for i in range(3, int(original_num / 2) + 1, 2):
         # این لوپ برای چک کردن این که ایا عدد تست شده به عدد گرفته شده توسط کاربر بخش پذیر است یا خیر
         # تا وقتی تمام ضریب های این توابع پیدا نشده سراغ چک کردن عدد بعدی نمی رود
-        while reminder:
-            # وقتی عدد مورد نظر به یک میرسد پی دیگر عامل دیگری ندارد و میتوان جواب را برگرداند
+        # وقتی عدد مورد نظر به یک میرسد پی دیگر عامل دیگری ندارد و میتوان جواب را برگرداند
+        while True:
             if number == 1:
                 return avamel
-
-            elif number % i == 0:
-                number /= i
-                if avamel.get(f'{i}'):
-                    avamel[f'{i}'] += 1
-                else:
-                    avamel[f'{i}'] = 1
+            temp_num = number / i
+            if f'{temp_num}'[-1] == '0':
+                if not avamel.get(f'{i}'):
+                    avamel[f'{i}'] = 0
+                avamel[f'{i}'] += 1
+                number = temp_num
             else:
-                reminder = False
+                break
+
     return avamel
 
 
