00c30c341512c2400b7b494df09ce3e179203391
pedram.zamaninezhad <pzamaninejad@yahoo.com>
2024-01-03 20:49:44 +0330

    90% question 6 solved

--- question_6.py
+++ question_6.py
@@ -1,13 +1,17 @@
-def find_prependicular(two_point: list):
+def find_prependicular(point_one: tuple, point_two: tuple):
     """
-    :param two_point: [(x1, y1), (x2, y2)]
+    :param point_one: first point ; (x1, y1)
+    :param point_two: second point ; (x2, y2)
     :return: (m, h) : slop and Width from the origin ; mX + h
     """
-    mid_x = (two_point[0][0] + two_point[1][0]) / 2
-    mid_y = (two_point[0][1] + two_point[1][1]) / 2
+    mid_x = (point_one[0] + point_two[0]) / 2
+    mid_y = (point_one[1] + point_two[1]) / 2
     try:
-        prependicular_slop = (-1) / ((two_point[0][1] - two_point[1][1]) / (two_point[0][0] - two_point[1][0]))
+        prependicular_slop = (-1) / ((point_one[1] - point_two[1]) / (point_one[0] - point_two[0]))
     except ZeroDivisionError:
+        # cheking for infinit slop
+        if point_one[1] - point_two[1] == 0:
+            return ((point_two[0] + point_two[0]) / 2, )
         prependicular_slop = 0
     # عرض از مبدا
     h = mid_y - (prependicular_slop * mid_x)
@@ -39,10 +43,14 @@ def impact_prependicular_rectangle(rectangle: tuple, prependicular: tuple):
     if prependicular[1] <= rectangle[1]:
         impacted.append((0, prependicular[1]))
 
+    for i in impacted:
+        if i[0] > rectangle[0] or i[1] > rectangle[1] or i[0] < 0 or i[1] < 0:
+            impacted.remove(i)
+
     return impacted
 
 
-def impact_point_two_prependucular(prependicular1: tuple, prependicular2: tuple):
+def impact_prependucular(prependicular1: tuple, prependicular2: tuple):
     """
     :param prependicular1: (m, h) : slop and Width from the origin ; mX + h
     :param prependicular2: (m, h) : slop and Width from the origin ; mX + h
@@ -54,7 +62,36 @@ def impact_point_two_prependucular(prependicular1: tuple, prependicular2: tuple)
     return impact_x, impact_y
 
 
+def main_idea(points: list, rectangle: tuple):
+    """
+    :param points: the points cordinents
+    :param rectangle: the rectangle top left cordinent
+    :return: چند ضلعی
+    """
+    prependicular = []
+    for i in range(len(points)):
+        try:
+            prependicular.append(find_prependicular(points[i], points[i + 1]))
+        except IndexError:
+            prependicular.append(find_prependicular(points[i], points[0]))
+    importans_point = impact_prependucular(prependicular[0], prependicular[1])
+    impacts = [impact_prependicular_rectangle(rectangle, prependicular[x]) for x in range(len(prependicular))]
+    for i in range(len(impacts)):
+        thrid_point = (i + 2) % 3  # نقطه مورد نیازه سوم برای مقایسه فاصله برخورد عمود منصف
+        for j in range(len(impacts[i])):
+            distance1_1 = ((impacts[i][j][0] - points[thrid_point][0]) ** 2 + (impacts[i][j][1] - points[thrid_point][1]) ** 2) ** 0.5
+            distance1_2 = ((impacts[i][j][0] - points[i][0]) ** 2 + (impacts[i][j][1] - points[i][1]) ** 2) ** 0.5
+            if distance1_1 < distance1_2:
+                impacts[i].pop(j)
+                break
+
+    print(f'important point --> {importans_point}')
+    print(f'Impacts --> {impacts}')
+
+
 if __name__ == '__main__':
-    points = [(2, 3), (2, 6)]
+    points = [(2, 3), (2, 6), (4, 7)]
     Rectangle = (6, 10)
-    print(impact_prependicular_rectangle(Rectangle, find_prependicular(points)))
+    main_idea(rectangle=Rectangle, points=points)
+    # print(find_prependicular(points[0], points[1]))
+    # print(find_prependicular((2, 3), (4, 3)))
